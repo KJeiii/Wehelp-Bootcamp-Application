65b117ab8afc2e8259a3a16b7b0abaa9c56ebad5
KJeiii <109458440+KJeiii@users.noreply.github.com>
2023-04-15 21:58:03 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+from flask import Flask, url_for, render_template
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+Bootstrap(app)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/project')
+def project():
+    return render_template('project.html')
+
+@app.route('/schedule')
+def schedule():
+    return render_template('schedule.html')
+
+@app.route('/company')
+def company():
+    return render_template('company.html')
+
+@app.route('/negative')
+def negative():
+    return render_template('negative.html')
+
+@app.route('/reflection')
+def reflection():
+    return render_template('reflection.html')
+
+@app.route('/effort')
+def effort():
+    return render_template('effort.html')
+
+@app.route('/talktalk')
+def talktalk():
+    return render_template('talktalk.html')
+
+app.run(debug=True)
